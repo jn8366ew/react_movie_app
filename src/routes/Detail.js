44e593b3fb197635905996aed8cb54adbd010b04
jn8366ew <jn8366ew@mnsu.edu>
2021-10-08 03:35:44 -0500

    Added routes

---
+++ src/routes/Detail.js
@@ -0,0 +1,31 @@
+import React from "react";
+
+
+
+// function Detail({location}){
+//     console.log(location);
+//     return <span>dgffbf</span>
+// }
+
+class Detail extends React.Component {
+    componentDidMount() {
+        const {location, history} = this.props
+        if(location.state === undefined){
+            history.push("/");
+        }
+
+    }
+    render() {
+        const {location} = this.props;
+        if(location.state) {
+            return <span>{location.state.title}</span>;
+        } else {
+            return null;
+        }
+        return <span>{location.state.title}</span>;
+    }
+}
+
+
+
+export default Detail;
