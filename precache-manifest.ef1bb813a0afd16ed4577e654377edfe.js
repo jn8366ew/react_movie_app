8890e6a0dbc1fc373a2a47c58ec4679b3a763ead
jn8366ew <jn8366ew@mnsu.edu>
2021-10-08 01:23:40 -0500

    Updates

---
+++ precache-manifest.ef1bb813a0afd16ed4577e654377edfe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12b88c0a3bf90773932b62c040147196",
+    "url": "/react_movie_app/index.html"
+  },
+  {
+    "revision": "ea1a1f626c432811616e",
+    "url": "/react_movie_app/static/css/main.67fa9561.chunk.css"
+  },
+  {
+    "revision": "00476a8e1c728e7292b2",
+    "url": "/react_movie_app/static/js/2.a117c034.chunk.js"
+  },
+  {
+    "revision": "fb6fca4f0fa26a7e27d26480a74532c9",
+    "url": "/react_movie_app/static/js/2.a117c034.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea1a1f626c432811616e",
+    "url": "/react_movie_app/static/js/main.c7049639.chunk.js"
+  },
+  {
+    "revision": "51fad4c4d744f2e68f63",
+    "url": "/react_movie_app/static/js/runtime-main.99b3fb45.js"
+  }
+]);
